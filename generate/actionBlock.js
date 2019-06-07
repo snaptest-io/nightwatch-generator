16a6978805f99e7789831acab231138628c93a01
Joseph Jung <joseph@hologram.io>
2019-06-06 23:33:33 -0500

    Ignore some warnings due to actions not being required.

--- generate/actionBlock.js
+++ generate/actionBlock.js
@@ -10,9 +10,13 @@ var ActionsByConstant = require('../static/actiondata').ActionsByConstant;
 module.exports = function(test, testData, indent) {
 
   test.actions.forEach((action) => {
-    if (!actions[action.type]) {
+
+    var ignoredWarnings = ["PUSHSTATE", "POPSTATE"];
+
+    if (!actions[action.type] && ignoredWarnings.indexOf(action.type) === -1) {
       console.warn(`WARNING: The "${ActionsByConstant[action.type] ? ActionsByConstant[action.type].name : action.type}" action in test "${test.name}" is not implemented and will be skipped.`);
     }
+
   });
 
   var rootBlock = arrangeActionsIntoBlocks(test.actions);
